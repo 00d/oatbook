f283d336d4a85d864227d30704f2fff67dc16e21
John Britzke <ziggynose@gmail.com>
2025-08-20 23:22:54 -0700

    base url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:4444/api/"
+    : "/api/";
 const instance = axios.create({
-    baseURL: "http://localhost:4444/api/",
+  baseURL: BASE_URL,
 });
 
 export default instance;
